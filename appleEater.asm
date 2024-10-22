f39091c58a3467ea56258a8f625b13624251b071
Briarey <Briarey@gmail.ru>
2024-10-22 20:57:48 +0300

    write total count

--- appleEater.asm
+++ appleEater.asm
@@ -3,6 +3,9 @@
 
 section .data
 	tableSize	equ TABLE_WIDTH * TABLE_HEIGHT + TABLE_HEIGHT
+	bufferSize	equ 5
+	total_count_str	db	"TOTAL COUNT: "
+	total_count_len equ	$ - total_count_str
 	table_char	db	'#'
 	apple_char	db	'*'
 	eater_char	db	'@'
@@ -10,17 +13,44 @@ section .data
 	space_char	db 	' ' 
 	eaterXPos	db	1
 	eaterYPos	db	1
+	appleXPos	db	0
+	appleYPos	db	0
+	appleGenerated	db	0
+	total_count	db	0
 section .bss 
 	table 		resb tableSize
 	inputChar	resb 1
+	buffer		resb bufferSize
 section .text 
 global _start
 _start:
 game:
 	clear_term
+
+	cmp	byte [appleGenerated], 0
+	je	apple_draw
+
+check_eater_and_apple_coords:
+	mov	al, [eaterXPos]
+	cmp	al, [appleXPos]
+	jne	table_draw
+	mov	al, [eaterYPos]
+	cmp	al, [appleYPos]
+	jne	table_draw
+	jmp 	apple_draw
+
+apple_draw:
+	call setRandAppleValues
+	mov	byte [appleGenerated], 1
+
+table_draw:
 	call fill_table 
 
 	print_str table, tableSize 
+	print_str new_line_char, 1
+	print_str total_count_str, total_count_len
+	print_num [total_count], buffer, bufferSize
+	print_str new_line_char, 1
 	
 input:
 	read_char inputChar
@@ -119,18 +149,32 @@ center:
 
 space_line:
 	inc	ebx
-
 	xor	eax, eax
 	mov	al, byte [eaterYPos] 
 	dec	eax
 	cmp	eax, esi
-	jne	space
+	jne	apple
 	xor	eax, eax
 	mov	al, byte [eaterXPos]
 	dec	eax
 	cmp	eax, edi
-	jne	space
+	jne	apple
 	mov	eax, [eater_char]
+	jmp	cont
+
+apple:
+	xor	eax, eax
+	mov	al, byte [appleYPos] 
+	dec	eax
+	cmp	eax, esi
+	jne	space
+	xor	eax, eax
+	mov	al, byte [appleXPos]
+	dec	eax
+	cmp	eax, edi
+	jne	space	
+	mov	eax, [apple_char]
+	inc	byte [total_count]
 	jmp	cont
 
 space:
@@ -176,4 +220,34 @@ bottom_line:
 	pop 	ebx
 	pop 	eax
 	ret
-;
+
+
+
+setRandAppleValues:
+	push	eax
+	push	ebx
+	push 	edx
+
+	rdtsc			;get TSC value into eax 
+	and	eax, 0x7FFFFFFF	;left only 31 bit
+	mov	ebx, TABLE_WIDTH - 2
+	xor	edx, edx
+	div	ebx
+	inc	edx		;get value from 1 to TABLE_WIDTH - 2
+	mov	[appleXPos], edx
+
+
+	rdtsc			
+	and	eax, 0x7FFFFFFF
+	mov	ebx, TABLE_WIDTH - 2
+	xor	edx, edx
+	div	ebx
+	inc	edx	
+	mov	[appleYPos], edx
+	
+	pop	edx
+	pop	ebx
+	pop	eax
+
+	ret
+
